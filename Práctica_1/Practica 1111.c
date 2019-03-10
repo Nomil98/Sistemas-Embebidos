74e96400ba64dea1b8604804b6329cd3a8ccb17e
Nomil98 <47511927+Nomil98@users.noreply.github.com>
2019-03-10 13:09:52 -0600

    Update Practica 1111.c
    Había un espacio extra innecesario entre un punto y coma en la variable contador al momento de ser dividida.

--- Práctica_1/Practica 1111.c
+++ Práctica_1/Practica 1111.c
@@ -1,9 +1,9 @@
-#include<18F4620.h> //Se incluye la librerÌa del PIC 18F4620
+#include<18F4620.h> //Se incluye la librer√≠a del PIC 18F4620
 #fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT // EL HS es el high speed
 #fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
 #use delay (clock = 16000000)//frecuencia de reloj en 16 millones
 //Como nos podemos dar cuenta nuestro 
-//clock est· funcionando en modo HS 
+//clock est√° funcionando en modo HS 
 //Por la frencuencia en la que trabaja.
 
 void main(void){
@@ -11,8 +11,8 @@ setup_oscillator(OSC_16MHZ); //se establece el clock otra vez.
 set_tris_b(0x00);//Con este  comando establecemos  los puertos b en modo salida.
 int contador = 128; //La variable.
 while(1){
-    output_b(contador); //La salida que tendr· el PIC.
-    contador /= 2 ;
+    output_b(contador); //La salida que tendr√° el PIC.
+    contador /= 2;
     if (contador == 0)
     contador = 128;
     delay_ms(500);
