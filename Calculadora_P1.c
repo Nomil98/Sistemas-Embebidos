ff673dcb6c91ba72291aa33d395443dd6a705380
Nomil98 <47511927+Nomil98@users.noreply.github.com>
2019-03-20 08:33:58 -0600

    Update Calculadora_P1.c
    Hicimos un cambio de puerto de E3 por el  D7.

--- Calculadora_P1.c
+++ Calculadora_P1.c
@@ -17,7 +17,7 @@ void main (void){
    set_tris_a(0xcf);
    set_tris_b(0xff);
    set_tris_c(0x00);
-   set_tris_d(0x00);
+   set_tris_d(0x80);
    set_tris_e(0xf);
    int op=0;
    while(1){
@@ -30,7 +30,7 @@ void main (void){
            op=2;
         if(input(PIN_E2) ==1)
            op=3;
-        if(input(PIN_E3) ==1)
+        if(input(PIN_D7) ==1)
            op=4;
         
         if(input(PIN_E0)==1 && op==1){
@@ -45,7 +45,7 @@ void main (void){
            resultado = valor1 * valor2;
            op=0;
    }
-          if(input(PIN_E3)==1 && op==4){
+          if(input(PIN_D7)==1 && op==4){
        if(input_b()!=0){
           resultado= valor1/ valor2;
           op=0;
@@ -56,4 +56,5 @@ void main (void){
    output_c(resultado);
    output_d(resultado>>8);
    }
+   
 }
