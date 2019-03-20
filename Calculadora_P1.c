2dda444b98ef247fdb05165d7918030ed0daac28
Nomil98 <47511927+Nomil98@users.noreply.github.com>
2019-03-20 17:20:28 -0600

    Update Calculadora_P1.c
    Se anexaron las condiciones para marcar el error e ingresar números negativos (aún en proceso ).
    .

--- Calculadora_P1.c
+++ Calculadora_P1.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
-#fuses NOPBADEN, /*NOMCLR,*/ STVREN, NOLVP, NODEBUG
+#fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
 #use delay(clock=16000000)
 
 #use fast_io(a)
@@ -10,8 +10,8 @@
 #use fast_io(c)
 #use fast_io(d)
 #use fast_io(e)
-long  valor1 = 0, valor2 = 0, resultado = 0;
-
+long  operando1 = 0, operando2 = 0, resultado = 0;
+void rebound(void);
 void main (void){
    setup_oscillator(OSC_16MHZ);
    set_tris_a(0xcf);
@@ -19,42 +19,64 @@ void main (void){
    set_tris_c(0x00);
    set_tris_d(0x80);
    set_tris_e(0xf);
-   int op=0;
+   int operacion=0;
    while(1){
-       valor1 = input_a();
-       valor2 = input_b();
-   
-        if(input(PIN_E0) ==1)
-           op=1;
-        if(input(PIN_E1) ==1)
-           op=2;
-        if(input(PIN_E2) ==1)
-           op=3;
-        if(input(PIN_D7) ==1)
-           op=4;
-        
-        if(input(PIN_E0)==1 && op==1){
-           resultado = valor1 + valor2; 
-           op=0;
-      }
-        if(input(PIN_E1)==1 && op==2){
-           resultado = valor1 - valor2;
-           op=0;
-     }
-        if(input(PIN_E2)==1 && op==3){
-           resultado = valor1 * valor2;
-           op=0;
-   }
-          if(input(PIN_D7)==1 && op==4){
-       if(input_b()!=0){
-          resultado= valor1/ valor2;
-          op=0;
+       operando1 = input_a();
+       operando2 = input_b();
+       
+       if(input(PIN_B7)==1){
+           operando1*= -1;
        }
-           
+        if(input(PIN_D6)==1){
+           operando2*= -1;
+       }
+       
+        if(input(PIN_E0) ==1)
+            operacion=1;a
+        if(input(PIN_E1) ==1)
+            operacion=2;
+        if(input(PIN_E2) ==1)
+            operacion=3;
+        if(input(PIN_D7) ==1)
+            operacion=4;
+        
+        if(input(PIN_E0)==1 && operacion==1){
+            resultado = operando1 + operando2; 
+                operacion=0;
+      }
+        if(input(PIN_E1)==1 && operacion==2){
+            resultado = operando1 - operando2;
+                operacion=0;
+     }
+        if(input(PIN_E2)==1 && operacion==3){
+            resultado = operando1 * operando2;
+                operacion=0;
+   }
+          if(input(PIN_D7)==1 && operacion==4){
+              if(input_b()!=0){
+                  resultado= operando1/ operando2;
+                      operacion=0;
+ }
+       else{
+           rebound();
+       }
            
    }
    output_c(resultado);
    output_d(resultado>>8);
    }
-   
 }
+
+void rebound(void){
+   long Error = 0x0;
+   for(int parpadeo = 1; parpadeo < 4; parpadeo++){
+      Error = 0x1fff;
+      output_c(Error);
+      output_d(Error>>8);
+      delay_ms(250);
+      Error = 0x0;
+      output_c(Error);
+      output_d(Error>>8);
+      delay_ms(250);
+   }
+}    
